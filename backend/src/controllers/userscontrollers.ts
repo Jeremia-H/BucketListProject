1a1bd8991ccec8104b4666c642875d791cba96ef
Jeremia Henkel <henkel.jeremia@gmail.com>
2024-08-06 21:57:04 +0200

    user api methods

--- backend/src/controllers/userscontrollers.ts
+++ backend/src/controllers/userscontrollers.ts
@@ -1,8 +1,8 @@
 import { RequestHandler } from "express";
 import user from "../models/user";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
-export const getUser: RequestHandler = async (req, res, next) => {
+const getUser: RequestHandler = async (req, res, next) => {
   try {
     const userdata = await user.find().exec(); //With this we get the data from the database
     res.status(200).json(userdata); // this catches the response from the await above as ok and then gives out the sensordata as json
@@ -12,20 +12,84 @@ export const getUser: RequestHandler = async (req, res, next) => {
   }
 };
 
-export const createUser: RequestHandler = async (req, res, next) => {
+const createUser: RequestHandler = async (req, res, next) => {
   try {
     const { username, password } = req.body; // get the username and password from the request body
     const hashedpass = await bcrypt.hash(password, 10); // hash the password with bcrypt
-    console.log(hashedpass)
-      const newUser = await user.create({ username,password: hashedpass }); // create a new user with the username and password
-      res.status(201).json(newUser); // send the new user as json
-      console.log(newUser.username), + "created";
-      return newUser;
+    console.log(hashedpass);
+    const newUser = await user.create({ username, password: hashedpass }); // create a new user with the username and password
+    res.status(201).json(newUser); // send the new user as json
+    console.log(newUser.username), +"created";
+    return newUser;
   } catch (error) {
-
-      next(error);
+    next(error);
   }
 };
+
+const editUser: RequestHandler = async (req, res, next) => {
+  try {
+        const { username, password, newpassword } = req.body;
+        //check if username or password or both were provided
+        if (!username || !password) 
+          {
+            res.status(400).json({ message: "Please provide username and password" });
+            console.log("Please provide username and password");
+            return;
+          }
+        const newUserData = await user.findOne({ username: username }).exec();
+        console.log(newUserData?.username + " found");
+        if (!newUserData?.password) 
+          {
+            console.log("User not found");
+            return;
+          }
+        const isPasswordCorrect = await bcrypt.compare(password, newUserData.password);
+        if (!isPasswordCorrect) 
+          {
+            res.status(401).json({ message: "Invalid password" });
+            console.log("Invalid password");
+            return;
+          }
+        console.log("Password correct");
+        await newUserData.updateOne({password: await bcrypt.hash(newpassword, 10)});
+        console.log(newUserData.password);
+        res.status(200).json({ message: "Password changed" });
+      } 
+  catch (error) {
+    next(error);
+  }
+};
+
+const deleteUser: RequestHandler = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      res.status(400).json({ message: "Please provide username and password" });
+      console.log("Please provide username and password");
+      return;
+    }
+    const newUserData = await user.findOne({ username: username }).exec();
+    console.log(newUserData?.username + " found");
+    if (!newUserData?.password) {
+      console.log("User not found");
+      return;
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, newUserData.password);
+    if (!isPasswordCorrect) {
+      res.status(401).json({ message: "Invalid password" });
+      console.log("Invalid password");
+      return;
+    }
+    console.log("Password correct");
+    await newUserData.deleteOne();
+    res.status(200).json({ message: "User deleted" });
+  }
+  catch (error) {
+    next(error);
+  }
+};
+
+export { getUser, createUser, editUser,deleteUser };
 
 /**
  * Explain in Documentation:
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -4,6 +4,8 @@ import * as UserData from "../controllers/userscontrollers";
 const router = express.Router(); //create a router object
 router.get("/users", UserData.getUser);
 router.post("/users", UserData.createUser);
+router.patch("/users", UserData.editUser);
+router.delete("/users", UserData.deleteUser);
 
 export default router;
 
